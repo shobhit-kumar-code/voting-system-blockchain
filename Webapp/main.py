3e70a1930dc3b9be27afc5051541b17501d2f479
lordbeerus0505 <abhiram.natarajan@gmail.com>
2019-08-03 19:25:30 +0530

    added validations

--- Webapp/main.py
+++ Webapp/main.py
@@ -4,28 +4,40 @@ import json
 from flask import Flask, render_template, request
 import sys
 from vote_system import Voting
-sys.path.append('D:\EmotionDetection')
+# sys.path.append('D:\EmotionDetection')
 # print(sys.path)
 # from Mails import Mail
 
 app = flask.Flask(__name__,static_url_path='/static')
 
-@app.route("/")
-def home():
-    result={'emotions':"sent",'response':"recv"}
-    return flask.render_template("home.html",result=result)
+# @app.route("/")
+# def home():
+#     result={'emotions':"sent",'response':"recv"}
+#     return flask.render_template("home.html",result=result)
 
 
 @app.route("/register_candidate")
 def register_candidate():
     return flask.render_template("register_candidate.html")
-@app.route("/registration_complete",methods = ['POST', 'GET'])
-def registration_complete():
+@app.route("/register_voter")
+def register_voter():
+    return flask.render_template("register_voter.html")
+@app.route("/registration_complete_candidate",methods = ['POST', 'GET'])
+def registration_complete_candidate():
     if request.method=="POST":
       result=request.form
       obj=Voting()
-      obj.register_candidate(result['uid'],result['fname'],result['lname'],result['age'],result['address'],result['photo'],result['criminal'])
-      return flask.render_template("registration_complete.html",result=result)
+      if obj.register_candidate(result['uid'],result['fname'],result['lname'],result['age'],
+          result['address'],result['photo'],result['criminal']) ==True:
+            return flask.render_template("registration_complete.html",result=result)
+@app.route("/registration_complete_voter",methods = ['POST', 'GET'])
+def registration_complete_voter():
+    if request.method=="POST":
+      result=request.form
+      obj=Voting()
+      if obj.register_voter(result['uid'],result['fname'],result['lname'],result['age'],
+          result['address'],result['photo']) ==True:
+            return flask.render_template("registration_complete.html",result=result)
 if __name__ == "__main__":
 
     app.run(debug=True)
--- Webapp/vote_system.py
+++ Webapp/vote_system.py
@@ -1,4 +1,5 @@
 import pymongo
+import re
 class Voting:
     uid,fname,lname,age,address,photo,criminal_records=('',)*7
     def __init__(self):
@@ -8,22 +9,42 @@ class Voting:
         self.age=21
         self.address="default"
         self.photo="pic.jpg"
-        self.criminal_records="default"
+        
     #perform validation on the front end itself and carry on.
     def register_candidate(self,uid,fname,lname,age,address,photo,criminal_records): #additional details are needed for this operation.
-        self.uid,self.fname,self.lname,self.age=uid,fname,lname,age
-        self.register_db()
+        self.uid,self.fname,self.lname,self.age,self.address,self.photo,self.criminal_records=uid,fname,lname,age,address,photo,criminal_records
+        return (self.validate("candidate") and self.register_db("candidate"))
+        
         #make call to blockchain registercandidate function
-    def register_voter(self,uid,fname,lname,age):
-        self.uid,self.fname,self.lname,self.age=uid,fname,lname,age
+    def validate(self,category):
+        # import pdb; pdb.set_trace()
+        if category=="candidate":
+            if re.match(r'\b\d{12}\b',self.uid) and int(self.age)>=25 and self.address!="default" and self.fname!="default" and self.lname!="default" and self.photo!="pic.jpg":
+                return True
+            else:
+                return False
+        else:
+            if re.match(r'\b\d{12}\b',self.uid) and int(self.age)>=18 and self.address!="default" and self.fname!="default" and self.lname!="default" and self.photo!="pic.jpg":
+                return True
+            else:
+                return False
+    def register_voter(self,uid,fname,lname,age,address,photo):
+        self.uid,self.fname,self.lname,self.age,self.address,self.photo=uid,fname,lname,age,address,photo
+        return (self.validate("voter") and self.register_db("voter"))
         #make call to blockchain registervoter function
     def cast_vote(self,uid,fname,lname,vote_uid):
         #make call to blockchain cast_vote function with true for vote variable
         pass
-    def register_db(self):
-        import pdb; pdb.set_trace()
+    def register_db(self,category):
+        # import pdb; pdb.set_trace()
         myclient = pymongo.MongoClient("mongodb://localhost:27017/")
         mydb = myclient["codefundo"]
-        mycol=mydb['cand_reg']
-        mycol.insert_one({"UID":self.uid,"First Name":self.fname,"Last Name":self.lname,"Age":self.age,"Address":self.address,
+        if category=="candidate":
+            mycol=mydb['cand_reg']
+            mycol.insert_one({"UID":self.uid,"First Name":self.fname,"Last Name":self.lname,"Age":self.age,"Address":self.address,
                         "Photo":self.photo,"Criminal":self.criminal_records})
+        else:
+            mycol=mydb['vote_reg']
+            mycol.insert_one({"UID":self.uid,"First Name":self.fname,"Last Name":self.lname,"Age":self.age,"Address":self.address,
+                        "Photo":self.photo})
+        return True
