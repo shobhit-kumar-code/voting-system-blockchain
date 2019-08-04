6c2a899f6eff3c4223a96605eb0d002cb5348fd6
DipayanSinha <romulus.sinha@gmail.com>
2019-08-04 11:19:22 +0530

    Added basic UI

--- Webapp/main.py
+++ Webapp/main.py
@@ -10,26 +10,30 @@ from vote_system import Voting
 
 app = flask.Flask(__name__,static_url_path='/static')
 
-# @app.route("/")
-# def home():
-#     result={'emotions':"sent",'response':"recv"}
-#     return flask.render_template("home.html",result=result)
+@app.route("/")
+def home():
+     return flask.render_template("home.html")
 
 
 @app.route("/register_candidate")
 def register_candidate():
     return flask.render_template("register_candidate.html")
+
 @app.route("/register_voter")
 def register_voter():
     return flask.render_template("register_voter.html")
+
 @app.route("/registration_complete_candidate",methods = ['POST', 'GET'])
 def registration_complete_candidate():
     if request.method=="POST":
       result=request.form
+      print(result['uid'],result['fname'],result['lname'],result['age'],
+          result['address'],result['photo'],result['criminal'])
       obj=Voting()
       if obj.register_candidate(result['uid'],result['fname'],result['lname'],result['age'],
           result['address'],result['photo'],result['criminal']) ==True:
             return flask.render_template("registration_complete.html",result=result)
+
 @app.route("/registration_complete_voter",methods = ['POST', 'GET'])
 def registration_complete_voter():
     if request.method=="POST":
