0db69fc5303b3cd23cef5763e967685d734447b2
lordbeerus0505 <abhiram.natarajan@gmail.com>
2019-08-13 17:00:25 +0530

    Fixed front end bugs

--- Webapp/main.py
+++ Webapp/main.py
@@ -1,16 +1,22 @@
 import flask
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 import sys
 from werkzeug import secure_filename
 from vote_system import Voting
 import pdb
 from azurenews import NewsSearch
-
 import cv2
 import pymongo
+import requests
 
+# app.secret_key = 'development'
+session={}
+applid=4
+candidateRoleId=7
+voteWorkflowPropertyId=7
+currentuserid=-1
 # sys.path.append('D:\EmotionDetection')
 # print(sys.path)
 # from Mails import Mail
@@ -68,7 +74,8 @@ def registration_complete_candidate():
       obj=Voting()
       if obj.register_candidate(result['uid'],result['fname'],result['lname'],result['age'],
           result['address'],result['gender'],result['wardno'],str(file_handler.filename),result['criminal']) ==True:
-            return flask.render_template("registration_complete.html",result=result,mapping=mapping,photo="../static/PurpleAdmin-Free-Admin-Template-master/images/"+file_handler.filename,visa=None)
+          # return redirect("https://login.microsoftonline.com/kumarshobhit98outlook.onmicrosoft.com/oauth2/authorize?response_type=id_token%20code&client_id=c80344c2-d7fc-41e1-adcc-dd33683a7f6b&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fregister&state=c0756113-6172-47f2-8afc-666f315c15b1&client-request-id=0de0f9e0-a2f4-4853-9bd2-7326f1f409d1&x-client-SKU=Js&x-client-Ver=1.0.17&nonce=3f993c47-3042-4669-bdce-02024f6c802f&response_mode=form_post")  
+          return flask.render_template("registration_complete.html",result=result,mapping=mapping,photo="../static/PurpleAdmin-Free-Admin-Template-master/images/"+file_handler.filename,visa=None)
       else:
         return flask.render_template("permission_denied_voter.html")
 @app.route("/registration_complete_voter",methods = ['POST', 'GET'])
@@ -182,6 +189,247 @@ def voted():
     mycol.find_one_and_update({"UID":whom},{'$inc':{"vote_count":1}})
     # import pdb; pdb.set_trace()
     return flask.render_template("thank_you.html",result=mycol.find_one({"UID":whom}))
+
+
+##################################################################################################################
+##################################################################################################################
+#########################################################
+#########################################################
+#########################################################
+#########################################################
+
+
+def getyourroleid():
+  global currentuserid
+  url="https://votemaadi-4bm4ew-api.azurewebsites.net/api/v1/applications/"+str(applid)+"/roleAssignments"
+  # params={'workflowId':1,'contractCodeId':1,'connectionId':1}
+  headers={'Authorization': 'Bearer {0}'.format(session['auth_token'])}#{'Content-Type': 'application/json',
+  
+  responsefromapi = requests.get(url,headers=headers)
+  print(responsefromapi.url)
+  print(responsefromapi.status_code)
+  print(responsefromapi.json())
+  if responsefromapi.status_code == 200:
+      results=json.loads(responsefromapi.content.decode('utf-8'))
+      print('user role code is')
+      x=0
+      while results['roleAssignments'][x]['user']['userID']!=currentuserid:
+          print(results['roleAssignments'][x]['user']['userID'])
+          print(currentuserid)
+          x=x+1
+      print(results['roleAssignments'][x]['applicationRoleId'])
+      if results['roleAssignments'][x]['applicationRoleId']==candidateRoleId:
+          return 'candidate'
+      else:
+          return 'voter'
+
+def getusertype():
+  global currentuserid
+  url="https://votemaadi-4bm4ew-api.azurewebsites.net/api/v1/users/me"
+  # params={'workflowId':1,'contractCodeId':1,'connectionId':1}
+  headers={'Authorization': 'Bearer {0}'.format(session['auth_token'])}#{'Content-Type': 'application/json',
+  
+  responsefromapi = requests.get(url,headers=headers)
+  print(responsefromapi.url)
+  print(responsefromapi.status_code)
+  print(responsefromapi.json())
+  if responsefromapi.status_code == 200:
+      results=json.loads(responsefromapi.content.decode('utf-8'))
+      print('hello')
+      print(results['currentUser']['userID'])
+      currentuserid=results['currentUser']['userID']
+      if results['capabilities']['canUpgradeWorkbench']==True:
+          return 'admin'
+      else:
+          
+          return 'user'
+      
+  else:
+      return 'failed at getting user type'
+def getvotecount():
+  url="https://votemaadi-4bm4ew-api.azurewebsites.net/api/v1/contracts?workflowId=4"
+  # params={'workflowId':1,'contractCodeId':1,'connectionId':1}
+  headers={'Authorization': 'Bearer {0}'.format(session['auth_token'])}#{'Content-Type': 'application/json',
+  
+  responsefromapi = requests.get(url,headers=headers)
+  print(responsefromapi.url)
+  print(responsefromapi.status_code)
+  print(responsefromapi.json())
+  if responsefromapi.status_code == 200:
+      results=json.loads(responsefromapi.content.decode('utf-8'))
+      return results
+  else:
+      return 'error in getting votes'
+@app.route("/getvotes",methods = ['POST', 'GET'])
+def getvotes():
+  ansstr={}
+  votejson= getvotecount()
+  for x in votejson['contracts']:
+      # f=0
+      temp =  'dude number :'+str(x['id'])+':'
+      for y in x['contractProperties']:
+          if y['workflowPropertyId']==14:
+              temp = temp + 'number of votes: '+str(y['value'])+'\n'
+              ansstr[str(x['id'])]=y['value']
+              # f=1
+      # if f==1:
+      #     ansstr = ansstr+temp
+  return ansstr
+
+      
+@app.route("/shobhit",methods = ['POST', 'GET'])
+def shobhit():
+  
+  data = request.form.to_dict()
+  print(data['id_token'])
+  auth_token=data['id_token']
+  session['auth_token']=auth_token
+  usertype = getusertype()
+  if usertype=='user':
+      usertype1=getyourroleid()
+      if usertype1=='voter':
+        cap = cv2.VideoCapture(0)
+        obj=Voting()
+        import time
+        while True:
+          time.sleep(3)
+          ret, frame = cap.read()
+          cv2.imwrite("img.jpg",frame)
+          if obj.check_emotion():
+            myclient=pymongo.MongoClient(uri)
+            mydb = myclient["codefundo"]
+            mycol=mydb['cand_reg']
+            result=[]
+            for x in mycol.find():
+              result.append(x)
+            # import pdb; pdb.set_trace()
+            return flask.render_template("cast_vote.html",result=result)
+          else:
+              return flask.render_template("permission_denied_emotion.html")
+          break
+          # return render_template('cast_vote.html')
+      else:
+          return render_template('candidatehome.html')
+  else:
+      return render_template('adminhome.html')
+@app.route("/launchcandidate",methods = ['POST', 'GET'])
+def launchcandidate():
+  apidata={
+      "workflowFunctionID": 8,
+      "workflowActionParameters": [
+        {
+          "name": "uid",
+          "value": "12",
+          "workflowFunctionParameterId": 6
+        }
+      ]
+    }
+  print(apidata)
+  url="https://votemaadi-4bm4ew-api.azurewebsites.net/api/v1/contracts?workflowId=4&contractCodeId=4&connectionId=1"
+  # params={'workflowId':1,'contractCodeId':1,'connectionId':1}
+  headers={'Authorization': 'Bearer {0}'.format(session['auth_token'])}#{'Content-Type': 'application/json',
+  
+  responsefromapi = requests.post(url,json=apidata,headers=headers)
+  print(responsefromapi.url)
+  print(responsefromapi.status_code)
+  if responsefromapi.status_code == 200:
+      results=json.loads(responsefromapi.content.decode('utf-8'))
+      newcontract=results
+      print('newcontract')
+      print(newcontract)##add to db
+      ##TODO add this dude lauched
+  return 'tolaunchcandidate'
+@app.route("/reguser",methods = ['POST', 'GET'])
+def reguser():
+  # candidate_uid=request.form['uid']
+  #add this UID to Mongodb for thr candidate, and verify it is unique, if not send back to /ext
+  #all this comes from adminhome.html
+
+  apidata={
+      "externalID": request.form['externalid'],
+      "firstName": request.form['firstname'],
+      "lastName": request.form['lastname'],
+      "emailAddress": request.form['emailid']
+    }
+  print(apidata)
+  url="https://votemaadi-4bm4ew-api.azurewebsites.net/api/v1/users"
+  # params={'workflowId':1,'contractCodeId':1,'connectionId':1}
+  headers={'Authorization': 'Bearer {0}'.format(session['auth_token'])}#{'Content-Type': 'application/json',
+  
+  responsefromapi = requests.post(url,json=apidata,headers=headers)
+  print(responsefromapi.url)
+  print(responsefromapi.status_code)
+  if responsefromapi.status_code == 200:
+      results=json.loads(responsefromapi.content.decode('utf-8'))
+      newuser=results
+
+      apidata={
+          "userId": newuser,
+          "applicationRoleId": 7 #role for candidate
+        }
+      print(apidata)
+      url="https://votemaadi-4bm4ew-api.azurewebsites.net/api/v1/applications/"+str(applid)+"/roleAssignments"
+      # params={'workflowId':1,'contractCodeId':1,'connectionId':1}
+      headers={'Authorization': 'Bearer {0}'.format(session['auth_token'])}
+
+      responsefromapitoassignrole = requests.post(url,json=apidata,headers=headers)
+
+      return 'thanks for registering'
+  else:
+      return 'failed'
+
+
+@app.route("/shobhit_voted",methods = ['POST', 'GET'])
+def shobhit_voted():
+  candidate_uid=request.form['uid']
+
+
+  apidata={"workflowFunctionID": 9,"workflowActionParameters": []}
+  url="https://votemaadi-4bm4ew-api.azurewebsites.net/api/v1/contracts/"+str(candidate_uid)+"/actions"
+  # params={'workflowId':1,'contractCodeId':1,'connectionId':1}
+  headers={'Authorization': 'Bearer {0}'.format(session['auth_token'])}#{'Content-Type': 'application/json',
+  
+  responsefromapi = requests.post(url,json=apidata,headers=headers)
+  print(responsefromapi.url)
+  print(responsefromapi.status_code)
+  if responsefromapi.status_code == 200:
+      results=json.loads(responsefromapi.content.decode('utf-8'))
+      ##TODO DB add this dude has voted
+      return 'thanks for voting'
+  else:
+      return 'failed'
+
+  return 'thanks for voting'
+
+@app.route("/shobhit1",methods = ['POST', 'GET'])
+def shobhit1():
+  data = request.form.to_dict()
+  print(data['id_token'])
+  # print(request.form)
+
+  apidata={"workflowFunctionID": 1,"workflowActionParameters": [  { "name": "message", "value": "lalala", "workflowFunctionParameterId": 3 } ] }
+  auth_token=data['id_token']
+  url="https://votemaadi-4bm4ew-api.azurewebsites.net/api/v1/contracts"
+  params={'workflowId':1,'contractCodeId':1,'connectionId':1}
+  headers={'Authorization': 'Bearer {0}'.format(auth_token)}#{'Content-Type': 'application/json',
+  
+  responsefromapi = requests.post(url,params=params, json=apidata,headers=headers)
+  print(responsefromapi.url)
+  print(responsefromapi.status_code)
+  if responsefromapi.status_code == 200:
+      newid=json.loads(responsefromapi.content.decode('utf-8'))
+      return 'a'
+  else:
+      return 'hello'
+
+  return 'hello'
+
+
+@app.route("/ext")
+def ext():    
+  # print(data['id_token'])
+  return redirect("https://login.microsoftonline.com/kumarshobhit98outlook.onmicrosoft.com/oauth2/authorize?response_type=id_token%20code&client_id=c80344c2-d7fc-41e1-adcc-dd33683a7f6b&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fshobhit&state=c0756113-6172-47f2-8afc-666f315c15b1&client-request-id=0de0f9e0-a2f4-4853-9bd2-7326f1f409d1&x-client-SKU=Js&x-client-Ver=1.0.17&nonce=3f993c47-3042-4669-bdce-02024f6c802f&response_mode=form_post")
+  # return redirect("https://login.microsoftonline.com/kumarshobhit98outlook.onmicrosoft.com/oauth2/v2.0/authorize?client_id=c62087b9-cfed-4105-a9c2-4fd3953ceed5&response_type=id_token&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fshobhit&response_mode=fragment&scope=openid&state=12345&nonce=678910")
 
 
 if __name__ == "__main__":
