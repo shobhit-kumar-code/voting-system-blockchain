866cd113fcb9488d2bda8481362246dd1853faf4
lordbeerus0505 <abhiram.natarajan@gmail.com>
2019-08-13 23:57:47 +0530

    Changes to system, new files added with new functionality

--- Webapp/main.py
+++ Webapp/main.py
@@ -31,8 +31,22 @@ def home():
     obj=NewsSearch()
     result=obj.news()
     return flask.render_template("home.html",result=result)
+@app.route("/home",methods = ['POST', 'GET'])
+def home_1():
+    obj=NewsSearch()
+    result=obj.news()
+    return flask.render_template("redirect_home.html",result=result)
 
-
+@app.route("/list")
+def list_fun():
+  myclient=pymongo.MongoClient(uri)
+  mydb = myclient["codefundo"]
+  mycol=mydb['vote_reg']
+  result=[]
+  for x in mycol.find():
+    result.append(x)
+  return flask.render_template("electoral.html",result=result)
+  
 @app.route("/register_candidate")
 def register_candidate():
     return flask.render_template("register_candidate.html")
@@ -160,10 +174,15 @@ def cast_vote_home():
       mydb = myclient["codefundo"]
       mycol=mydb['cand_reg']
       result=[]
+      obj=NewsSearch()
       for x in mycol.find():
         result.append(x)
+      news=[]
       # import pdb; pdb.set_trace()
-      return flask.render_template("cast_vote.html",result=result)
+      for res in result:
+        news.append(obj.news(str(res["First Name"]+" "+res['Last Name']+" india election")))
+      # import pdb; pdb.set_trace()
+      return flask.render_template("cast_vote.html",result=result,news=news)
     else:
         return flask.render_template("permission_denied_emotion.html")
     break
@@ -473,6 +492,11 @@ def ext():
   # print(data['id_token'])
   return redirect("https://login.microsoftonline.com/kumarshobhit98outlook.onmicrosoft.com/oauth2/authorize?response_type=id_token%20code&client_id=c80344c2-d7fc-41e1-adcc-dd33683a7f6b&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fregister_msft&state=c0756113-6172-47f2-8afc-666f315c15b1&client-request-id=0de0f9e0-a2f4-4853-9bd2-7326f1f409d1&x-client-SKU=Js&x-client-Ver=1.0.17&nonce=3f993c47-3042-4669-bdce-02024f6c802f&response_mode=form_post")
   # return redirect("https://login.microsoftonline.com/kumarshobhit98outlook.onmicrosoft.com/oauth2/v2.0/authorize?client_id=c62087b9-cfed-4105-a9c2-4fd3953ceed5&response_type=id_token&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fshobhit&response_mode=fragment&scope=openid&state=12345&nonce=678910")
+
+@app.route("/redirect_home")
+def ext1():    
+  # print(data['id_token'])
+  return redirect("https://login.microsoftonline.com/kumarshobhit98outlook.onmicrosoft.com/oauth2/authorize?response_type=id_token%20code&client_id=c80344c2-d7fc-41e1-adcc-dd33683a7f6b&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fhome&state=c0756113-6172-47f2-8afc-666f315c15b1&client-request-id=0de0f9e0-a2f4-4853-9bd2-7326f1f409d1&x-client-SKU=Js&x-client-Ver=1.0.17&nonce=3f993c47-3042-4669-bdce-02024f6c802f&response_mode=form_post")
 
 
 if __name__ == "__main__":
